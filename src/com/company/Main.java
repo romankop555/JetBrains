374146cfac56026d860aaa9cc5ed40968f919043
romankop555 <romankop555@gmail.com>
2020-11-16 23:04:26 +0300

    переменные

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,23 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        float a;
+    a = 12.5f;
+            System.out.println(a);
+    byte b = 56;
+        System.out.println(b);
+    short s = 3101;
+        System.out.println(s);
+        int i = 25456466;
+        System.out.println(i);
+        long l = 455343434535463L;
+        System.out.println(l);
+        double d=61514646.5455;
+        System.out.println(d);
+        char c =  '\u4fdf';
+        System.out.println(c);
+        boolean bl = true;
+        bl = false;
 
     }
 }
